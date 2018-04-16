59d36307646591697c31a411dbb3f5f170948707
unknown <ranny.kaddoura@powercoders.org>
2018-04-16 10:47:45 +0200

    Login user page first commit

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,37 @@
+import { User } from './../shared/user.model';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+
+  user: User;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+[.]+[a-z]{2,4}$";
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+
+  resetForm(form? : NgForm) {
+
+    if(form != null)
+    form.reset();
+    this.user = {
+      UserName: '',
+      Password: '',
+      Email: '',
+      FirstName: '',
+      LastName: ''
+    }
+  }
+
+}
